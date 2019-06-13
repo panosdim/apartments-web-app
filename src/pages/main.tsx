a408d85576e2198e14d2e71367a908e3d9671999
Panagiotis Dimopoulos <panosdim@gmail.com>
2019-06-13 23:43:45 +0300

    Add NavBar and Table for Flats

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { H1, Card, H3, Elevation, Button, Intent, H4, H5 } from '@blueprintjs/core';
+import { Card, H3, Elevation, Button, Intent, Navbar, Alignment, HTMLTable } from '@blueprintjs/core';
 import { useGlobal, setGlobal } from 'reactn';
 import axios from 'axios';
+import { FlatType } from '../model';
 
 export const Main: React.FC = () => {
     const [user] = useGlobal('user');
@@ -24,19 +25,48 @@ export const Main: React.FC = () => {
         setGlobal({ isLoggedIn: false });
     };
 
+    const handleSelection = (flat: FlatType) => {
+        console.log(flat);
+    };
+
     return (
         <>
-            <H1 style={{ textAlign: 'center' }}>Apartments Management App</H1>
-            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                <Card interactive={true} elevation={Elevation.TWO} style={{ width: 'max-content' }}>
-                    <H3>User Info</H3>
-                    <H4>{user.first_name + ' ' + user.last_name}</H4>
-                    <H5>{user.email}</H5>
+            <Navbar style={{ marginBottom: '16px' }}>
+                <Navbar.Group align={Alignment.LEFT}>
+                    <Navbar.Heading>Apartments Management</Navbar.Heading>
+                </Navbar.Group>
+                <Navbar.Group align={Alignment.RIGHT}>
+                    <Navbar.Heading>{user.first_name + ' ' + user.last_name}</Navbar.Heading>
+                    <Navbar.Divider />
                     <Button icon='log-out' intent={Intent.DANGER} text='Logout' onClick={logout} />
-                </Card>
-                <Card interactive={true} elevation={Elevation.TWO} style={{ width: 'max-content' }}>
+                </Navbar.Group>
+            </Navbar>
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                <Card
+                    interactive={true}
+                    elevation={Elevation.TWO}
+                    style={{ width: 'max-content', height: 'max-content' }}
+                >
                     <H3>Flats Management</H3>
-                    {flats && flats.map(flat => <H4>{flat.name + ' ' + flat.address + ' ' + flat.floor}</H4>)}
+                    <HTMLTable interactive={true} condensed={true}>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Address</th>
+                                <th>Floor</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {flats &&
+                                flats.map(flat => (
+                                    <tr key={flat.id} onClick={() => handleSelection(flat)}>
+                                        <td>{flat.name}</td>
+                                        <td>{flat.address}</td>
+                                        <td>{flat.floor}</td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </HTMLTable>
                 </Card>
             </div>
         </>
